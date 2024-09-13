a69098d5b98b6862dc743fdc2063b34e0d261edc
Adnan Ashraf <ashrafadnan959@gmail.com>
2024-09-13 11:22:59 +0530

    fixed: bugs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -35,15 +35,15 @@ export const Home = () => {
             </AnimatedGradientText>
           </div>
         </div>
-        <div className="side  relative hover:scale-105 shadow-2xl transition-all duration-300  backdrop-blur-lg backdrop-saturate-150 bg-black/15 border-2 border-[rgba(255, 255, 255, 0.125)] z-30 -mt-24 rounded-lg  h-[30vh] w-[80vw]">
+        <div className="side h-[40vh] -mt-32 flex flex-col justify-center  items-center relative hover:scale-105 shadow-2xl transition-all duration-300  backdrop-blur-lg backdrop-saturate-150 bg-black/15 border-2 border-[rgba(255, 255, 255, 0.125)] z-30 -mt-24 rounded-lg  h-[30vh] w-[80vw]">
           <div className="title p-2 flex items-center justify-center">
-            <h1 className="text-sm text-wrap font-mono text-center text-white  md:text-xl lg:text-2xl">
+            <h1 className="text-sm text-wrap font-mono -mt-20 text-center text-white  md:text-xl lg:text-2xl">
               Our most loved services
             </h1>
           </div>
           <div className="elements  flex items-center justify-center  h-[20vh] w-full">
-            <div className="items w-full">
-              <ul className="flex gap-2 md:justify-around justify-around  p-4 text-white text-wrap text-sm">
+            <div className="items mt-5 flex flex-col justify-around w-full">
+              <ul className="flex items-center gap-2 md:justify-around justify-around h-fit  p-4 text-white text-wrap text-sm">
                 <li className="w-1/3 hover:scale-125 transition-all duration-300 hover:text-cyan-600 md:text-xl text-center flex gap-2 text-white flex-col justify-center items-center">
                   <img
                     className=" mix-blend-color-burn"
@@ -52,7 +52,7 @@ export const Home = () => {
                     width={50}
                     alt="Web Developement"
                   />
-                  <p className="text-xs md:text-lg lg:text-xl font-mono">
+                  <p className="text-xs md:text-lg lg:text-lg font-mono">
                     Web Designing & Development
                   </p>
                 </li>
@@ -64,7 +64,7 @@ export const Home = () => {
                     width={50}
                     alt="Social Media"
                   />
-                  <p className="text-xs md:text-lg lg:text-xl font-mono">
+                  <p className="text-xs md:text-sm lg:text-lg font-mono">
                     Social Media Management
                   </p>
                 </li>
@@ -76,12 +76,12 @@ export const Home = () => {
                     width={50}
                     alt="Content Writing"
                   />
-                  <p className="text-xs md:text-lg lg:text-xl font-mono">
-                    Video Editing
+                  <p className="text-xs md:text-sm lg:text-lg font-mono">
+                    Content Creation and Editing
                   </p>
                 </li>
               </ul>
-              <p className="text-center h-fit w-fit m-auto">
+              <p className="text-center mt-12 h-fit w-fit m-auto">
                 <AnimatedGradientText>
                   <Link to="services" smooth={true} duration={500}>
                     <span
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -29,35 +29,35 @@ export function Services() {
           </h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={social} alt="Web" height={50} width={50}/>
+        <img src={social} alt="social" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Social Media</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={ai} alt="Web" height={50} width={50}/>
+        <img src={ai} alt="Ai" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Ai Services</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={market} alt="Web" height={50} width={50}/>
+        <img src={market} alt="marketing" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Marketing</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={music} alt="Web" height={50} width={50}/>
+        <img src={music} alt="Music" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Music & Audio</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={graphic} alt="Web" height={50} width={50}/>
+        <img src={graphic} alt="Graphic" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Designing</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={video} alt="Web" height={50} width={50}/>
+        <img src={video} alt="Video" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Video Editing</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={write} alt="Web" height={50} width={50}/>
+        <img src={write} alt="Write" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Content Writing & Translation</h3>
         </div>
         <div className="card-shine-effect hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-3  w-full mb-10 md:mb-0 md:w-[25vw]">
-        <img src={data} alt="Web" height={50} width={50}/>
+        <img src={data} alt="Data" height={50} width={50}/>
           <h3 className="title text-center text-lg md:text-xl">Data Analysis</h3>
         </div>
       </div>
